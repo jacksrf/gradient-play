173430ec1f5e94458f79424d6f32f04c5990e57d
Trey Jackson <treyjackson@TreyJacksonMBP.lan>
2022-08-26 13:45:57 -0500

    updates

--- gradient.js
+++ gradient.js
@@ -3,7 +3,7 @@ $(window).on('load', function() {
   var slider = $('.taglines_slider').slick({
     dots: true,
     infinite: true,
-    speed: 1000,
+    speed: 750,
     fade: true,
     cssEase: 'linear',
     arrows: false,
@@ -11,23 +11,9 @@ $(window).on('load', function() {
     autoplaySpeed: 4000
   });
 
-  // $('.change_circles').click(function() {
-  //   console.log('NEXT')
-  //   var lastID = $('.circles .circle:first').attr('id')
-  //   var newID = $('.circles .circle:last').attr('id')
-  //   $('.circles').prepend($('.circles .circle:last').addClass('tempLast'))
-  //   setTimeout(function() {
-  //     $('.circles .circle.tempLast').removeClass('tempLast')
-  //   }, 1)
-  //   $('body').removeClass(lastID)
-  //   $('body').addClass(newID)
-  // })
-
   $('.taglines_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
     var lastID = $('.circles .circle:first').attr('id')
-    console.log(lastID)
     var newID = $('.circles .circle:last').attr('id')
-    console.log(newID)
     $('.circles').prepend($('.circles .circle:last').addClass('tempLast'))
     setTimeout(function() {
       $('.circles .circle.tempLast').removeClass('tempLast')
